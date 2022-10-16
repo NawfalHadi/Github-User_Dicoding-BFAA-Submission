79de094bcdb87c4e95d97e59b328be7060de2528
Nawfal Hadi <nawfalhdi@gmail.com>
2022-10-16 20:42:58 +0800

    Additional Feature (open github via browser)

--- app/src/main/java/com/thatnawfal/githubuser/presentation/ui/profile/DetailUserActivity.kt
+++ app/src/main/java/com/thatnawfal/githubuser/presentation/ui/profile/DetailUserActivity.kt
@@ -1,6 +1,8 @@
 package com.thatnawfal.githubuser.presentation.ui.profile
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
@@ -8,36 +10,46 @@ import com.thatnawfal.githubuser.data.model.UserModel
 import com.thatnawfal.githubuser.databinding.ActivityDetailUserBinding
 import com.thatnawfal.githubuser.presentation.ui.home.MainActivity
 
+
 class DetailUserActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailUserBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.headerDetailUser.backButtonDetail.setOnClickListener {
+        val dataUser = intent.getParcelableExtra(MainActivity.EXTRA_KEY) as? UserModel
+        dataUser?.let { bindingView(it) }
+
+        binding.backButtonDetail.setOnClickListener {
             finish()
         }
 
-        val dataUser = intent.getParcelableExtra(MainActivity.EXTRA_KEY) as? UserModel
-        dataUser?.let { bindingView(it) }
+        binding.detailFbToGithub.setOnClickListener{
+            openGithubInBrowser(dataUser?.username)
+        }
     }
+
+    private fun openGithubInBrowser(username: String?) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse("https://github.com/${username}")
+        startActivity(i)
+    }
+
 
     @SuppressLint("SetTextI18n")
     private fun bindingView(dataUser: UserModel) {
         with(binding){
-            ivDetailAvatar.load(dataUser.avatar)
-            tvDetailNames.text = dataUser.name
-            tvDetailUsername.text = "@${dataUser.username}"
-
             with(followsDetailUser){
                 tvFollowers.text = dataUser.follower.toString()
                 tvFollowings.text = dataUser.following.toString()
             }
 
             with(headerDetailUser){
+                ivDetailAvatar.load(dataUser.avatar)
+                tvDetailNames.text = dataUser.name
+                tvDetailUsername.text = "@${dataUser.username}"
                 repoUserDetail.tvUserRepo.text = dataUser.repository.toString()
                 tvDetailLocation.text = dataUser.location
                 tvDetailCompany.text = dataUser.company
--- app/src/main/java/com/thatnawfal/githubuser/presentation/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/thatnawfal/githubuser/presentation/ui/splashscreen/SplashScreenActivity.kt
@@ -14,6 +14,7 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        @Suppress("DEPRECATION")
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
