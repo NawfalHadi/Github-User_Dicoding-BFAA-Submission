f9703d6f1698b24b4d4210e3a85b7bdc3190c76e
Nawfal Hadi <nawfalhdi@gmail.com>
2022-12-13 22:48:55 +0800

    depedency injection setup

--- app/src/main/java/com/thatnawfal/githubuser/presentation/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/thatnawfal/githubuser/presentation/ui/setting/SettingFragment.kt
@@ -17,7 +17,7 @@ class SettingFragment : BottomSheetDialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentSettingBinding.inflate(layoutInflater, container, false)
         return binding.root
